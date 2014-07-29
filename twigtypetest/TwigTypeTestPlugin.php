8bd65435a66d06d014017723333c2de38802ee1c
victor-in <victor-in@users.noreply.github.com>
2014-07-29 17:59:09 +0200

    v0.1

---
+++ twigtypetest/TwigTypeTestPlugin.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @package   Twig Type Test
+ * @author    Victor In.
+ * @copyright Copyright 2014
+ * @link      https://github.com/victor-in/Craft-TwigTypeTest
+ * @license   MIT
+ */
+namespace Craft;
+
+class TwigTypeTestPlugin extends BasePlugin
+{
+    function getName()
+    {
+         return Craft::t('Twig Type Test');
+    }
+
+    function getVersion()
+    {
+        return '0.1';
+    }
+
+    function getDeveloper()
+    {
+        return 'Victor In.';
+    }
+
+    function getDeveloperUrl()
+    {
+        return 'https://github.com/victor-in/';
+    }
+
+    function addTwigExtension()
+    {
+        Craft::import('plugins.twigtypetest.twigextensions.TwigTypeTestTwigExtension');
+        return new TwigTypeTestTwigExtension();
+    }
+}
---
+++ twigtypetest/twigextensions/TwigTypeTestTwigExtension.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Craft;
+
+class TwigTypeTestTwigExtension extends \Twig_Extension
+{
+	protected $env;
+
+	public function getName()
+	{
+		return 'Twig Type Test';
+	}
+
+	public function getTests()
+	{
+		return array('of_type' => new \Twig_Test_Method($this, 'twig_of_type'));
+	}
+	
+	public function getFilters()
+	{
+		return array('get_type' => new \Twig_Filter_Method($this, 'twig_get_type'));
+	}
+
+	public function initRuntime(\Twig_Environment $env)
+	{
+		$this->env = $env;
+	}
+
+	public function twig_of_type($var, $type_test=null)
+	{
+		
+		switch ($type_test)
+		{
+			default:
+				return false;
+				break;
+		
+			case 'array':
+				return is_array($var);
+				break;
+				
+			case 'bool':
+				return is_bool($var);
+				break;	
+		
+			case 'float':
+				return is_float($var);
+				break;
+				
+			case 'int':
+				return is_int($var);
+				break;
+				
+			case 'numeric':
+				return is_numeric($var);
+				break;
+				
+			case 'object':
+				return is_object($var);
+				break;
+				
+			case 'scalar':
+				return is_scalar($var);
+				break;
+				
+			case 'string':
+				return is_string($var);
+				break;
+		}
+	}
+	
+	public function twig_get_type($var)
+	{
+		return gettype( $var );
+	}
+	
+}
